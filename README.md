9d8068aa78877544a45e62fd452ed534275106fb
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-08-03 07:48:37 -0700

    Added server code

---
+++ README.md
@@ -0,0 +1,17 @@
+# snarky-hello
+
+## Purpose
+
+This API is HW #1 for The Node.js Master Class.
+
+I don't know what this is going to do yet.
+
+## Usage
+
+```
+POST /hello
+{
+    name: 'Phil'
+}
+```
+...get a personalized greeting in return.
---
+++ app/index.js
@@ -0,0 +1,84 @@
+/*
+ * snarky-hello: Returns a personalized greeting if you provide a name
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+const server = http.createServer((req, res) => {
+  // Process the request
+  const parsedUrl = url.parse(req.url, true);
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  const queryStringObject = parsedUrl.query;
+  const method = req.method.toLowerCase();
+
+  const headers = req.headers;
+
+  // Get the payload
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: helpers.parseJsonToObject(buffer)
+    };
+
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) === 'number' ? statusCode : 404;
+      payload = typeof(payload) === 'object' ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      // Send the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the path requested
+      if (statusCode === 200) {
+        console.log('\x1b[32m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+      } else {
+        console.log('\x1b[31m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+      }
+    });
+  });
+});
+
+const router = {
+  ping: (data, callback) => {
+    callback(200);
+  },
+  hello: (data, callback) => {
+    callback(200, { message: 'Hello' });
+  }
+};
+
+const helpers = {};
+
+helpers.parseJsonToObject = function(str) {
+  try {
+    const obj = JSON.parse(obj);
+    return obj;
+  } catch (e) {
+    return {};
+  }
+};
+
+server.listen(3000, () => { console.log('Listening on port 3000'); });
